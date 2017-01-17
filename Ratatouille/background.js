e79da91238002f2ded8a6288bbef28e582e34914
Lucas Folz <lfolz17@avenues.org>
2017-01-17 12:53:36 -0500

    Added background

---
+++ Ratatouille/background.js
@@ -0,0 +1,55 @@
+/*This background.js file will hold the main functionalitby of the program
+ *
+ *Program description:
+ *ADD HERE
+ *
+ */
+
+/*NOTES:
+A person responsible for this file should
+design the background as well as
+initalize the character and obstalce bby calling those functions and
+call the update functions for the character and the obstacle*/
+
+//Implement
+
+var bx1, bx2, bimg1, bimg2;
+
+function updateBackground(){
+  background(210,180,140);
+  
+	image(bimg1, bx1, 0);
+	image(bimg2, bx2, 0);
+  
+  bx1 -= 3;
+  bx2 -= 3;
+  
+  if (bx1 < -width) {
+    bx1 = width;
+  }
+  if (bx2 < -width) {
+    bx2 = width;
+  }
+}
+
+//Change or implement as needed
+function setup() {
+  createCanvas(1200, 515);
+  
+  bimg1 = loadImage('assets/background.jpeg');
+  bimg2= loadImage('assets/background.jpeg');
+  
+  bx1 = width;
+  bx2 = 0;
+  
+  initializeKnife();
+  
+  initializeRat();
+}
+
+//Change or implement as needed
+function draw() {
+  updateBackground();
+  updateRat();
+  updateKnife();
+}
