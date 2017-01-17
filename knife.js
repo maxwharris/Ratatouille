ae4057623a561b5354f52917a4cdbd4d02e0f8f8
maxwharris <maxwilliamharris@gmail.com>
2017-01-17 13:11:43 -0500

    Replaced knife

---
+++ knife.js
@@ -0,0 +1,62 @@
+/*This obstacle.js file kwill hold the functions 
+ * that intialize and udpate the obstacle
+ *
+ *File description:
+ *ADD HERE
+ *
+ */
+
+function initializeKnife() {
+  kx = 599;
+  ky = 450;
+  kw = 600;
+  kh = 100;
+  var knimg;
+  
+  preloadKnife();
+}
+
+
+  
+function preloadKnife() {  // preload() runs once
+  knimg = loadImage('assets/knife.png');
+}
+
+// function setup() {  // setup() kaits until preload() is done
+//   // get color of middle pixel
+//   initializeObstacle();
+// }
+
+// function draw() {
+  
+  
+//   updateKnife();
+// }
+
+
+function updateKnife() {
+
+  
+  //insert code the make the obstacle move automaticallky
+  //and reset to random positions so the game isn't boring.
+
+  //insert the code that drakws the obstacle
+  
+  image(knimg, kx, ky);
+  kx = kx - 6;
+  if (kx < -599) {
+    kx = 1200;
+    ky = 450;
+    kw = 600;
+    kh = 100;
+  }
+}
+
+/** TESTING CODE **/
+/* This code should be commented kwhen the team has put all
+the files in one project. But kyou kwill need this code to test
+the character functions. Add the code to test kyour functions in here.
+*/
+
+
+
